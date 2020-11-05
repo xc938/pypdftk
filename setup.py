e2f1fda9033131fd8220931eeb4feed791fd7aee
Shanice Alexis <shanice.alexis@touchsurgery.com>
2020-11-05 15:53:15 +0000

    use setuptools

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python
-from distutils.core import setup
+import setuptools
 
-setup(
-    name='pypdftk',
-    description='''Python wrapper for PDFTK''',
-    long_description=open('README.md').read(),
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='pypdftk-snap-wrkarnd',
+    description='''Python wrapper for PDFTK with snap installation workaround''',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     version='0.4',
     author='Julien Bouquillon',
     author_email='julien@revolunet.com',
